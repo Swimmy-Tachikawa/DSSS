63fea27da6a64e34baa0e93964b6767eee069d57
Swimmy-Tachikawa <swimmy_tachikawa001@sai.co.jp>
2026-03-04 17:49:01 +0900

    Add pyproject

---
+++ src/dsss/__init__.py
@@ -0,0 +1,8 @@
+"""
+gdrive_uploader: Local files to Google Drive uploader helper library.
+
+This package is designed to be UI/CLI agnostic.
+Drive operations are expected to be implemented via a separate adapter layer.
+"""
+
+__all__ = ["errors", "types"]
